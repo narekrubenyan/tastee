02615d00e9535f4cb32e17774565f266cfebe753
Davit Manucharyan <dmanucharyan30@gmail.com>
2024-07-23 22:01:24 +0400

    fixing bugs related to changing card

--- app/api/andpoints/cards.py
+++ app/api/andpoints/cards.py
@@ -139,86 +139,67 @@ def get_all_cards_by_user(current_user=Depends(security.get_current_user)):
                                                f"ERROR: {error}"})
 
 
-@card_router.put("/change-main-card/{card_id}")  #TODO
+@card_router.put("/change-main-card/{card_id}")
 def change_main_card(card_id: int, current_user=Depends(security.get_current_user)):
     user_id = dict(current_user).get("user_id")
 
     try:
-        main.cursor.execute("SELECT user_id FROM cards WHERE card_id=%s",
-                            (card_id,))
-
-        user_id_checked = main.cursor.fetchone()
+        main.cursor.execute("SELECT user_id FROM cards WHERE card_id=%s", (card_id,))
+        result = main.cursor.fetchone()
 
     except Exception as error:
         main.conn.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail={"message": f"There was an error getting user_id"
-                                               f"ERROR: {error}"})
+                            detail={"message": f"There was an error getting user_id. ERROR: {error}"})
 
-    if user_id_checked is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Card with ID {card_id} not found.")
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Card with ID {card_id} not found.")
 
-    if user_id_checked == user_id:
+    user_id_checked = result['user_id']
+
+    if user_id_checked != user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail={"message": "You do not have permission to delete this card."})
+                            detail={"message": "You do not have permission to change this card."})
 
     try:
-        main.cursor.execute("""SELECT card_id FROM cards WHERE user_id=%s AND status=%s""",
-                            (user_id, True))
-
-        card_id_old = main.cursor.fetchall()
+        main.cursor.execute("SELECT card_id FROM cards WHERE user_id=%s AND status=%s", (user_id, True))
+        result = main.cursor.fetchone()
 
     except Exception as error:
         main.conn.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail={"message": f"There was an error getting status"
-                                               f"ERROR: {error}"})
+                            detail={"message": f"There was an error getting the current main card. ERROR: {error}"})
 
-    if len(card_id_old) == 0:
+    card_id_old = result['card_id'] if result else None
+
+    if card_id_old is None:
         try:
-            main.cursor.execute("""
-                UPDATE cards SET status = %s 
-                WHERE card_id = %s""",
-                                (True, card_id))
-
+            main.cursor.execute("UPDATE cards SET status = %s WHERE card_id = %s", (True, card_id))
             main.conn.commit()
-
-            return JSONResponse(status_code=status.HTTP_200_OK,
-                                content={"message": f"{card_id} is basic card"},
-                                headers=headers)
+            return JSONResponse(status_code=status.HTTP_200_OK, content={"message": f"{card_id} is now the main card"})
 
         except Exception as error:
             main.conn.rollback()
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail={"message": f"There was an error updating status"
-                                                   f"ERROR: {error}"})
+                                detail={
+                                    "message": f"There was an error updating the status of {card_id}. ERROR: {error}"})
 
     try:
-        main.cursor.execute("""
-            UPDATE cards SET status=%s WHERE card_id=%s""",
-                            (False, card_id_old))
-
+        main.cursor.execute("UPDATE cards SET status = %s WHERE card_id = %s", (False, card_id_old))
         main.conn.commit()
 
     except Exception as error:
         main.conn.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail={"message": f"There was an error updating status of {card_id_old}",
-                                    "error": str(error)})
+                            detail={
+                                "message": f"There was an error updating the status of the old main card {card_id_old}. ERROR: {error}"})
 
     try:
-        main.cursor.execute("""
-            UPDATE cards SET status = %s 
-            WHERE card_id = %s""",
-                            (True, card_id))
+        main.cursor.execute("UPDATE cards SET status = %s WHERE card_id = %s", (True, card_id))
         main.conn.commit()
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"message": f"the main map was successfully changed"})
 
-        return JSONResponse(status_code=status.HTTP_200_OK,
-                            content={"message": f"{card_id} is basic card"},
-                            headers=headers)
     except Exception as error:
         main.conn.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail={"message": f"There was an error updating status of {card_id}",
-                                    "error": str(error)})
+                            detail={"message": f"There was an error updating the status of {card_id}. ERROR: {error}"})
